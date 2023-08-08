2daaa9e440516ddc30266975ec0cf0df8887c9d8
ovaomnis <ovaomnis@gmail.com>
2023-08-08 13:14:47 +0600

    fix

--- week11/class/larisa/models.py
+++ week11/class/larisa/models.py
@@ -7,4 +7,17 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
 
 
+class TestModel(models.Model):
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    integer = models.IntegerField()
+    in_stock = models.BooleanField(default=True)
+    email = models.EmailField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return str(self.pk)
+
+
 
